dab0d5ac8a07367b305c04773bd72954424223c1
Jonathan Thorpe <jon.thorpe@gmail.com>
2013-06-03 07:11:57 -0700

    fix

--- deploy/recipes/mysql.rb
+++ deploy/recipes/mysql.rb
@@ -1,15 +1,12 @@
-require 'resolv'
-
 cookbook_file "/tmp/mydb-init.sql" do
     source "mydb-init.sql"
 end
 
-execute "initialize mysql tables" do
-    command "/usr/bin/mysql -u#{deploy[:database][:username]}
--p#{deploy[:database][:password]} #{deploy[:database][:database]}  <
-/tmp/mydb-init.sql"
-      not_if "/usr/bin/mysql -u#{deploy[:database][:username]}
--p#{deploy[:database][:password]} #{deploy[:database][:database]}
--e'SHOW TABLES' | grep #{node[:phpapp][:dbtable]}"
+node[:deploy].each do |app_name, deploy|
+  execute "initialize mysql tables" do
+    command "/usr/bin/mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]}  < /tmp/mydb-init.sql"
+    not_if "/usr/bin/mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} -e'SHOW TABLES' | grep #{node[:phpapp][:dbtable]}"
+    action :run
+  end
 end
 
