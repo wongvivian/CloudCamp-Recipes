12002fbdea6440c92446e353b0152414f77d7a1e
Jonathan Thorpe <jon.thorpe@gmail.com>
2013-06-03 07:23:00 -0700

    another try

--- deploy/recipes/mysql.rb
+++ deploy/recipes/mysql.rb
@@ -5,7 +5,6 @@ end
 node[:deploy].each do |app_name, deploy|
   execute "initialize mysql tables" do
     command "/usr/bin/mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]}  < /tmp/mydb-init.sql"
-    not_if "/usr/bin/mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} -e'SHOW TABLES' | grep #{node[:phpapp][:dbtable]}"
     action :run
   end
 end
