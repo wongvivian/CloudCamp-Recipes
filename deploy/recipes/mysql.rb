5892bda2017cdfc0200644bd4ae216e6c0a05b11
Jonathan Thorpe <jon.thorpe@gmail.com>
2013-06-04 07:17:17 -0700

    removed db creation

--- deploy/recipes/mysql.rb
+++ deploy/recipes/mysql.rb
@@ -2,11 +2,6 @@ cookbook_file "/tmp/campsite_php.sql" do
     source "campsite_php.sql"
 end
 
-execute "create database" do
-    command "mysqladmin -uroot -pchangeme create campsite"
-    action :run
-end
-
 execute "initialize mysql tables" do
     command "/usr/bin/mysql -uroot -pchangeme campsite  < /tmp/campsite_php.sql"
     action :run
