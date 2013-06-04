9f192c217c725066a644537fe0e836586fb51498
Jonathan Thorpe <jon.thorpe@gmail.com>
2013-06-03 22:38:43 -0700

    reconfigured database name and script name

--- deploy/recipes/mysql.rb
+++ deploy/recipes/mysql.rb
@@ -1,9 +1,9 @@
-cookbook_file "/tmp/mydb-init.sql" do
+cookbook_file "/tmp/campsite_php.sql" do
     source "campsite_php.sql"
 end
 
 execute "initialize mysql tables" do
-    command "/usr/bin/mysql -uroot -pchangeme cloudcamp  < /tmp/mydb-init.sql"
+    command "/usr/bin/mysql -uroot -pchangeme campsite  < /tmp/campsite_php.sql"
     action :run
 end
 
