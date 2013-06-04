a54081059625eb1d6b19f520604f9d16bf36c496
Jonathan Thorpe <jon.thorpe@gmail.com>
2013-06-03 23:19:21 -0700

    create campsite database

--- deploy/recipes/mysql.rb
+++ deploy/recipes/mysql.rb
@@ -2,6 +2,7 @@ cookbook_file "/tmp/campsite_php.sql" do
     source "campsite_php.sql"
 end
 
+mysqladmin -uroot -pchangeme create campsite
 execute "initialize mysql tables" do
     command "/usr/bin/mysql -uroot -pchangeme campsite  < /tmp/campsite_php.sql"
     action :run
